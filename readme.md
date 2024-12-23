9a241bf607dca16a0e95b9626c8d858dfbb4f810
lucasbaldezzari <lmbaldezzari@gmail.com>
2024-12-23 19:24:37 -0300

    Versión 1.1.0.

--- readme.md
+++ readme.md
@@ -14,6 +14,10 @@ Herramientas de procesamiento y análisis de señales de Electroencefalografía 
 
 ### Versiones
 
+#### Versión 1.1.0
+
+- Se agrega función *load_file* dentro de un nuevo módulo llamado *utils*. Permite cargar los archivos dentro de la carpeta *datasets*.
+
 #### Versión 1.0.1
 
 - Se corrige readme.md
--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ def version():
     with open('pyproject.toml') as f:
         return toml.load(f)['project']['version']
 
-# Encuentra automáticamente los archivos en 'neuroiatools/datasets/erders'
+# Encuentra automáticamente los archivos en 'neuroiatools/datasets'
 def find_package_data():
     package_data = {}
-    base_path = os.path.join("neuroiatools", "datasets", "erders")
+    base_path = os.path.join("neuroiatools", "datasets" )
     for root, _, files in os.walk(base_path):
         rel_root = os.path.relpath(root, "neuroiatools")
         package_data[rel_root.replace(os.path.sep, ".")] = files
@@ -30,7 +30,7 @@ setup(
     packages=find_packages(),
     install_requires=getRequirements(),
     include_package_data=True,  # Habilita la inclusión de datos no Python
-    package_data=find_package_data(),  # Agrega los archivos de erders
+    package_data=find_package_data(),  # Agrega los archivos dentro de datasets
     author='LUCAS BALDEZZARI',
     author_email='lmbaldezzari@gmail.com',
     description='Tools for EEG processing and analysis',
